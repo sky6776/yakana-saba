5b7f3a0c43cdc8570e284d7112f8914660eb38bb
sky_6776 <137915295+sky6776@users.noreply.github.com>
2023-09-25 17:43:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 0);
 });
 
-document.querySelector('.menu-btn').addEventListener('click', function(){
-    document.querySelector('.menu').classList.toggle('is-active');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menuToggle");
+    const menu = document.getElementById("menu");
+
+    menuToggle.addEventListener("click", function () {
+        menu.classList.toggle("active");
+        menuToggle.classList.toggle("active");
+    });
 });
