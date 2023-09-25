5618a393616a67031a6f41d4dce05b498e843bcc
sky_6776 <137915295+sky6776@users.noreply.github.com>
2023-09-25 14:17:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const animationContainer = document.querySelector(".animation-container");
+
+    setTimeout(() => {
+        animationContainer.style.opacity = "1";
+        animationContainer.style.animation = "none";
+
+        setTimeout(() => {
+            animationContainer.style.display = "none";
+
+            setTimeout(() => {
+                animationContainer.style.opacity = "0";
+            }, 3000);
+        }, 5000);
+    }, 0);
+});
+
 document.querySelector('.menu-btn').addEventListener('click', function(){
     document.querySelector('.menu').classList.toggle('is-active');
 });
